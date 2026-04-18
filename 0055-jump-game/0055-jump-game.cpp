08d9173785c29c99f45e02d97551382a9d3e8c3e
yuheng0203 <91184330+yuheng0203@users.noreply.github.com>
2026-04-18 22:53:13 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
+        int maxReach = 0;
         
-        for(int i = 0; i < n - 1; i++){
-            if(nums[i] == 0){
-                bool canPass = false;
-                
-                for(int j = i - 1; j >= 0; j--){
-                    if(nums[j] > i - j){
-                        canPass = true;
-                        break;
-                    }
-                }
-                
-                if(!canPass){
-                    return false;
-                }
+        for(int i = 0; i < nums.size(); i++){
+            if(i > maxReach){
+                return false;
             }
+            maxReach = max(maxReach, i + nums[i]);
         }
         
         return true;
